3cab55a821b066c6f8ab3e24509f1c8d72ec03b5
Henry Augustiano <henry.augustiano04@gmail.com>
2023-10-05 15:19:12 -0700

    add login test

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
-const app = require('../index'); 
-const User = require('../models/User'); 
+const app = require('../index');
+const User = require('../models/User');
 const bcrypt = require('bcrypt');
 
 const testEmail = 'jest@gmail.com';
@@ -49,6 +49,57 @@ describe('User Registration', () => {
     expect(res.status).toBe(400);
     expect(res.body.message).toBe('User already exists');
   });
+});
+
+describe('User Login', () => {
+  it('should successfuly login a user', async () => {
+    const userData = {
+      email: testEmail,
+      password: testPassword,
+    };
+
+    // Perform the login request
+    const res = await request(app)
+      .post('/api/users/login')
+      .send(userData);
+
+    // Expect a successful response
+    expect(res.status).toBe(200);
+    expect(res.body.message).toBe('Authentication successful');
+
+    // Expect the response to contain a token
+    expect(res.body.token).toBeTruthy();
+  });
+
+  it('should return login error if credentials are invalid', async () => {
+    const userData = {
+      email: testEmail,
+      password: 'invalidPassword',
+    };
+
+    const res = await request(app)
+      .post('/api/users/login')
+      .send(userData);
+
+    // Expect a login error response
+    expect(res.status).toBe(401);
+    expect(res.body.message).toBe('Authentication failed');
+  });
+
+  it('should return login error if user does not exist', async () => {
+    const userData = {
+      email: 'invalid@gmail.com',
+      password: 'invalidPassword',
+    };
+
+    const res = await request(app)
+      .post('/api/users/login')
+      .send(userData);
+
+    expect(res.status).toBe(401);
+    expect(res.body.message).toBe('No user found');
+  });
+  
 });
 
 describe('User Delete', () => {
